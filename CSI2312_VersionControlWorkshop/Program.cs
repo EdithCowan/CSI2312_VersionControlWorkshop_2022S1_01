73eac1481a778923795c932188b93bb33a4c5b64
mmasek <mmasek@HD71921025>
2023-04-28 10:26:40 +0800

    Re-setting things for 2023 S1

--- CSI2312_VersionControlWorkshop/Program.cs
+++ CSI2312_VersionControlWorkshop/Program.cs
@@ -4,9 +4,10 @@
 //
 // A simple program, with a number of placeholder functions that are not implemented
 // The project is to be hosted on hithub.
-// The goal is to split the class into multiple groups, each will clone the repository, implement one of the functions, test, and commit their changes.
+// The goal is to split the students into multiple groups, each will clone the repository, implement one of the functions, test, and commit their changes.
 //
 // Edith Cowan University, 2022 - Martin Masek
+// updated for 2023, S1 - Martin Masek
 
 namespace CSI2312_VersionControlWorkshop
 {
@@ -30,78 +31,6 @@ namespace CSI2312_VersionControlWorkshop
             for (int i = 0; i < data.Length; i++)
             {
                 data[i] =rand.Next(1,100);
-            }
-        }
-
-        static void Task03(int[] data)
-        {
-            // sort array
-            Array.Sort(data);
-        }
-
-        static void Task04(int[] data)
-        {
-            // declaring and initializing the array
-
-
-
-            // Sort array in ascending order.
-            Array.Sort(data);
-
-
-
-            // reverse array
-            Array.Reverse(data);
-
-
-
-        }
-        static void Task05(int[] data)
-        {
-            // Set all values to largest value
-
-            // Sort in descending order, largest number will be the first element
-            Array.Reverse(data);
-
-            int largest = data[0];
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                data[i] = largest;
-            }
-        }
-
-        static void Task06(int[] data)
-        {
-            // Set all values to smallest value
-
-            // Sort in ascending order, smallest number will be the first element
-            Array.Sort(data);
-
-            int smallest = data[0];
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                data[i] = smallest;
-            }
-        }
-
-        static void Task07(int[] data)
-        {
-            // set all to total
-
-            int total = 0;
-
-            // getting the total
-            for (int i = 0; i < data.Length; i++)
-            {
-                total += data[i];
-            }
-
-            // setting all values as the total
-            for (int i = 0; i < data.Length; i++)
-            {
-                data[i] = total;
             }
         }
 
@@ -155,21 +84,6 @@ namespace CSI2312_VersionControlWorkshop
                         break;
                     case 2:
                         Task02(data);
-                        break;
-                    case 3:
-                        Task03(data);
-                        break;
-                    case 4:
-                        Task04(data);
-                        break;
-                    case 5:
-                        Task05(data);
-                        break;
-                    case 6:
-                        Task06(data);
-                        break;
-                    case 7:
-                        Task07(data);
                         break;
                     default:
                         break;
